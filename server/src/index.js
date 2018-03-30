4163c8e9eb2d69e276cc7a5dd0a93e115b4a7a53
Vyacheslav Borovsky <viachaslau.barouski@itechart-group.com>
2018-03-30 15:37:02 +0300

    Added cluster

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,27 @@
 const expressServer = require('./config/express');
 const config = require("./config/variable");
 const {initWebSocket} = require("./config/websocket");
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const PORT = config.common.port || 5555;
 
-const appServer = expressServer.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-});
+if (cluster.isMaster) {
+    console.error(`Node cluster master ${process.pid} is running`);
 
-initWebSocket(appServer);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-module.exports = appServer;
+    cluster.on('exit', (worker, code, signal) => {
+        console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    });
+
+} else {
+    const PORT = config.common.port || 5555;
+
+    const appServer = expressServer.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+    });
+
+    initWebSocket(appServer);
+}
