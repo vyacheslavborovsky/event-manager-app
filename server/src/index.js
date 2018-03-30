4652997b2cf86b508053e19e758414f42806e4f8
Vyacheslav Borovsky <viachaslau.barouski@itechart-group.com>
2018-03-30 17:33:25 +0300

    Removed cluster

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,7 @@
 const expressServer = require('./config/express');
 const config = require("./config/variable");
 const {initWebSocket} = require("./config/websocket");
-const cluster = require('cluster');
 const runNotificationJob = require("./api/utils/usersNotifyProcess").runNotificationJob;
-const numCPUs = require('os').cpus().length;
 
 const PORT = config.common.port || 5555;
 
@@ -16,28 +14,3 @@ initWebSocket(appServer);
 if (config.mode === 'production') {
     runNotificationJob();
 }
-
-/*if (cluster.isMaster) {
-    console.error(`Node cluster master ${process.pid} is running`);
-
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-    });
-
-} else {
-    const PORT = config.common.port || 5555;
-
-    const appServer = expressServer.listen(PORT, () => {
-        console.log(`server is running on port ${PORT}`);
-    });
-
-    initWebSocket(appServer);
-
-    if (config.mode === 'production') {
-        runNotificationJob();
-    }
-}*/
